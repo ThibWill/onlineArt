b6ce21b9165f987811dc7f5bdc4ea51ed4df868f
ThibWill <thibault.willer@gmail.com>
2020-09-13 14:38:09 +0200

    refactoring ball

--- ball/ball.js
+++ ball/ball.js
@@ -4,8 +4,16 @@ window.onload = () => {
   const mainFrameWidth = mainFrame.width;
   const mainFrameHeight = mainFrame.height;
   const radiusBall = 30;
-  const colors = [["#facc70", 50], ["#de7a22", 20], ["#20948b", 70], ["#6ab187", 15]];
+  const nbBalls = Math.floor(mainFrameHeight / (radiusBall*2));
+  const colors = [["#facc70", 25], ["#de7a22", 10], ["#20948b", 30], ["#6ab187", 7]];
+  let intervals = new Array(nbBalls);
+  let infosBall = new Array(nbBalls);
 
+  for(let i=0; i < nbBalls; i++) {
+    infosBall[i] = [radiusBall, 1];
+  }
+
+  /* Draw a ball on the canavs */
   function drawBall(x , y, color) {
     ctx.beginPath();
     ctx.arc(x, y, 30, 0, 2*Math.PI);
@@ -13,32 +21,24 @@ window.onload = () => {
     ctx.fill();
   }
 
+  /* Move one ball for one pixel left or right*/
   function moveBall(y, numberBall, color) {
     let posBall = infosBall[numberBall][0];
     if(posBall === mainFrameWidth-radiusBall) { infosBall[numberBall][1] = -1; }
     if(posBall === radiusBall) { infosBall[numberBall][1] = 1; }
     ctx.clearRect(0, y-radiusBall, mainFrameWidth, 60);
-    infosBall[numberBall][0] += 2*infosBall[numberBall][1];
+    infosBall[numberBall][0] += infosBall[numberBall][1];
     drawBall(posBall, y, color);
   }
 
-  let infosBall = []; 
-  let intervals = [];
-  function start() {
-    const nbBalls = Math.floor(mainFrameHeight / (radiusBall*2));
-    for(let i=0; i < nbBalls; i++) {
-      infosBall.push([radiusBall, 1]);
-      intervals.push(setInterval(() => moveBall(radiusBall + radiusBall*2*i, i, "black"), 10*(i+1)));
-    }
-    setInterval(changeColors, 10000);
-  }
-  start();
-  
+  /* Start the balls and change color */
   function changeColors() {
-    for(let i=0; i<intervals.length; i++) {
+    for(let i=0; i<nbBalls; i++) {
       let rand = Math.floor(Math.random()*colors.length);
       clearInterval(intervals[i]);
       intervals[i] = setInterval(() => moveBall(radiusBall + radiusBall*2*i, i, colors[rand][0]), colors[rand][1]);
     }
   }
+  changeColors();
+  setInterval(changeColors, 10000);
 }
