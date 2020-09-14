9d886904ffbe973b825760f81a58767fef440d3f
ThibWill <thibault.willer@gmail.com>
2020-09-14 10:37:19 +0200

    canavs arrows

--- arrow/arrow.js
+++ arrow/arrow.js
@@ -1,4 +1,35 @@
 window.onload = () => {
+  const arrowsFrame = document.getElementById("arrowsFrame");
+  const contextArrows = arrowsFrame.getContext("2d");
+
+  
+  function drawLine(xStart, yStart, xEnd, yEnd, ctx) {
+    ctx.beginPath();
+    ctx.moveTo(xStart,yStart);
+    ctx.lineTo(xEnd,yEnd);
+    ctx.stroke();
+  }
+
+  function drawFrameArrows() {
+    drawLine(30, 35, 45, 50, contextArrows);
+    drawLine(45, 50, 60, 35, contextArrows);
+    drawLine(45, 50, 45, 20, contextArrows);
+
+    drawLine(105, 20, 120, 35, contextArrows);
+    drawLine(120, 35, 105, 50, contextArrows);
+    drawLine(90, 35, 120, 35, contextArrows);
+
+    drawLine(150, 35, 165, 20, contextArrows);
+    drawLine(165, 20, 180, 35, contextArrows);
+    drawLine(165, 20, 165, 50, contextArrows);
+
+    drawLine(225, 20, 210, 35, contextArrows);
+    drawLine(210, 35, 225, 50, contextArrows);
+    drawLine(240, 35, 210, 35, contextArrows);
+  }
+
+  drawFrameArrows();
+
   const mainFrame = document.getElementById("mainFrame");
   const context = mainFrame.getContext("2d");
   const mainFrameWidth = mainFrame.width;
@@ -11,21 +42,14 @@ window.onload = () => {
   const offsetsUpLeft = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   const offsetsDownRight = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
 
-  function drawLines(y) { drawLine(0, y*60, mainFrameWidth, y*60); }
-  function drawCollumns(x) { drawLine(x*100, 0, x*100, mainFrameHeight); }
-
-  function drawLine(xStart, yStart, xEnd, yEnd) {
-    context.beginPath();
-    context.moveTo(xStart,yStart);
-    context.lineTo(xEnd,yEnd);
-    context.stroke();
-  }
+  function drawLines(y) { drawLine(0, y*60, mainFrameWidth, y*60, context); }
+  function drawCollumns(x) { drawLine(x*100, 0, x*100, mainFrameHeight, context); }
 
   function drawArrowUp(xStart, yStart) {
-    drawLine(xStart, yStart, xStart+50, yStart-20);
-    drawLine(xStart+50, yStart-20, xStart+100, yStart);
-    drawLine(xStart, yStart+20, xStart+50, yStart);
-    drawLine(xStart+50, yStart, xStart+100, yStart+20);
+    drawLine(xStart, yStart, xStart+50, yStart-20, context);
+    drawLine(xStart+50, yStart-20, xStart+100, yStart, context);
+    drawLine(xStart, yStart+20, xStart+50, yStart, context);
+    drawLine(xStart+50, yStart, xStart+100, yStart+20, context);
 
     /*drawLine(xStart+40, yStart-10, xStart+50, yStart-15);
     drawLine(xStart+50, yStart-15, xStart+60, yStart-10);*/
@@ -34,24 +58,24 @@ window.onload = () => {
   }
 
   function drawArrowDown(xStart, yStart) {
-    drawLine(xStart, yStart, xStart+50, yStart+20);
-    drawLine(xStart+50, yStart+20, xStart+100, yStart);
-    drawLine(xStart, yStart+20, xStart+50, yStart+40);
-    drawLine(xStart+50, yStart+40, xStart+100, yStart+20);
+    drawLine(xStart, yStart, xStart+50, yStart+20, context);
+    drawLine(xStart+50, yStart+20, xStart+100, yStart, context);
+    drawLine(xStart, yStart+20, xStart+50, yStart+40, context);
+    drawLine(xStart+50, yStart+40, xStart+100, yStart+20, context);
   }
   
   function drawArrowRight(xStart, yStart) {
-    drawLine(xStart, yStart, xStart+20, yStart+30);
-    drawLine(xStart+20, yStart+30, xStart, yStart+60);
-    drawLine(xStart+20, yStart, xStart+40, yStart+30);
-    drawLine(xStart+40, yStart+30, xStart+20, yStart+60);
+    drawLine(xStart, yStart, xStart+20, yStart+30, context);
+    drawLine(xStart+20, yStart+30, xStart, yStart+60, context);
+    drawLine(xStart+20, yStart, xStart+40, yStart+30, context);
+    drawLine(xStart+40, yStart+30, xStart+20, yStart+60, context);
   }
 
   function drawArrowLeft(xStart, yStart) {
-    drawLine(xStart, yStart, xStart-20, yStart+30);
-    drawLine(xStart-20, yStart+30, xStart, yStart+60);
-    drawLine(xStart+20, yStart, xStart, yStart+30);
-    drawLine(xStart, yStart+30, xStart+20, yStart+60);
+    drawLine(xStart, yStart, xStart-20, yStart+30, context);
+    drawLine(xStart-20, yStart+30, xStart, yStart+60, context);
+    drawLine(xStart+20, yStart, xStart, yStart+30, context);
+    drawLine(xStart, yStart+30, xStart+20, yStart+60, context);
   }
 
   function drawDirectionLeftRight(i, j, offset, direction) {
