e3a03a4a2c8e4edb5f4e45ae40c1b068431d9032
ThibWill <thibault.willer@gmail.com>
2022-02-02 22:57:20 +0100

    🚀clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,17 +1,37 @@
+/**
+ * @author Thibault Willer
+ * Date: 02/02/2022
+ */
 window.onload = () => {
 
     const MAX_HEIGHT = window.innerHeight;
     const MAX_WIDTH = window.innerWidth;
+    const CLOCK_RADIUS = MAX_HEIGHT / 2.1;
+    const INNER_CLOCK_RADIUS = MAX_HEIGHT / 2.25;
     const WIDTH_ARROW = (MAX_HEIGHT / 2.5);
     const HEIGHT_ARROW = 12;
+    const ARROW_COLORS = ["#ef545b", "#c0df98", "#6777be", "#9b7fb4", "#093727", "#b9eb7c"];
+
+    const ARROWS = Array(8).fill(0).map(e => {
+        return {
+            angle: (3 / 4) * 360,
+            color: ARROW_COLORS[Math.floor(Math.random() * ARROW_COLORS.length)]
+        }
+    });
 
     const canvas = document.createElement('canvas');
     canvas.height = MAX_HEIGHT;
     canvas.width = MAX_WIDTH;
     const context = canvas.getContext('2d');
-    context.font = '48px serif';
     document.body.append(canvas)
 
+    /**
+     * Draw an arrow on the canvas, starting at xStart, yStart
+     * @date 2022-02-02
+     * @param {number} xStrat
+     * @param {number} yStart
+     * @returns {void}
+     */
     function drawArrow(xStrat, yStart) 
     {
         context.beginPath();
@@ -24,36 +44,59 @@ window.onload = () => {
         context.fill();
     }
 
+    /**
+     * Draw the drame of the clock
+     * @date 2022-02-02
+     * @returns {void}
+     */
     function drawRound() 
     {
         context.save();
         context.beginPath();
-        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, MAX_HEIGHT / 2.1, 0, 2 * Math.PI, true); 
+        context.fillStyle = "#BA8C63";
+        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, CLOCK_RADIUS, 0, 2 * Math.PI, true); 
         context.fill();
         context.globalCompositeOperation = 'destination-out';
         context.beginPath();
-        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, MAX_HEIGHT / 2.25, 0, 2 * Math.PI, true);
+        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, INNER_CLOCK_RADIUS, 0, 2 * Math.PI, true);
         context.fill();
         context.restore();
     }
 
-    function drawNumbers() {
-        context.fillText('12', MAX_WIDTH / 2 - 24, MAX_HEIGHT / 4);
-        context.fillText('9', MAX_WIDTH / 4, MAX_HEIGHT / 2 - 24);
-        context.fillText('3', MAX_WIDTH / 2 - 24, MAX_HEIGHT / 4);
-        context.fillText('6', MAX_WIDTH / 2 - 24, 3 * MAX_HEIGHT / 4);
-    }
-
-    function drawCenter() 
-    {
+    /**
+     * Draw the grey lines in the frame of the clock to indicates main hours
+     * @date 2022-02-02
+     * @returns {void}
+     */
+    function drawLines() {
         context.save();
+        context.strokeStyle = "grey";
         context.beginPath();
-        context.fillStyle = "white";
-        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, 10, 0, 2 * Math.PI, true); 
-        context.fill();
+        context.moveTo(MAX_WIDTH / 2, (MAX_HEIGHT - 2 * INNER_CLOCK_RADIUS) / 2 + 50);
+        context.lineTo(MAX_WIDTH / 2, (MAX_HEIGHT - 2 * INNER_CLOCK_RADIUS) / 2 + 100);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(MAX_WIDTH / 2, (MAX_HEIGHT - (MAX_HEIGHT - 2 * INNER_CLOCK_RADIUS) / 2) - 50);
+        context.lineTo(MAX_WIDTH / 2, (MAX_HEIGHT - (MAX_HEIGHT - 2 * INNER_CLOCK_RADIUS) / 2) - 100);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(MAX_WIDTH - (MAX_WIDTH - 2 * INNER_CLOCK_RADIUS) / 2 - 100, MAX_HEIGHT / 2);
+        context.lineTo(MAX_WIDTH - (MAX_WIDTH - 2 * INNER_CLOCK_RADIUS) / 2 - 50, MAX_HEIGHT / 2);
+        context.stroke();
+        context.beginPath();
+        context.moveTo((MAX_WIDTH - 2 * INNER_CLOCK_RADIUS) / 2 + 50, MAX_HEIGHT / 2);
+        context.lineTo((MAX_WIDTH - 2 * INNER_CLOCK_RADIUS) / 2 + 100, MAX_HEIGHT / 2);
+        context.stroke();
         context.restore();
     }
 
+    /**
+     * Draw an arrow with an angle at the center of the clock
+     * @date 2022-02-02
+     * @param {number} angle
+     * @param {string} color
+     * @returns {void}
+     */
     function moveArrow(angle, color) 
     {
         context.save();
@@ -64,55 +107,97 @@ window.onload = () => {
         context.restore();
     }
 
+    /**
+     * Handle the calculation of the angle of an arrow
+     * @date 2022-02-02
+     * @returns {any}
+     */
     function angleArrow() 
     {
+        const VARIATION_VARIABLE_OFFSET = 0.002;
+        const RANGE_OFFSET = {
+            min: -0.2,
+            max: 2
+        };
+
         let variation = 0;
-        function variationArrow() {
-            variation += 0.5;
+        let way = true;
+        function variableOffset() {
+            if (variation < RANGE_OFFSET.min || variation > RANGE_OFFSET.max ) {
+                way = !way;
+            }
+            way ? variation += VARIATION_VARIABLE_OFFSET : variation -= VARIATION_VARIABLE_OFFSET;
+        }
+
+        function baseOffset(indiceArrow) {
+            return (indiceArrow / 5);
         }
 
         function computeOffsetAngleArrow(indiceArrow) {
-            return (indiceArrow / 5) + variation;
+            return baseOffset(indiceArrow) + variation;
         }
 
         return {
-            variationArrow,
+            variableOffset,
             computeOffsetAngleArrow
         }
     }
-    
 
-    function getRandomColor() 
-    {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+    const angleArr = angleArrow();
+    /**
+     * Draw all the arrows on the clock
+     * @date 2022-02-02
+     * @returns {any}
+     */
+    function drawArrows() {
+        angleArr.variableOffset();
+        for (let i = 1; i <= ARROWS.length; i++) {
+            moveArrow(ARROWS[i - 1].angle, ARROWS[i - 1].color);
+            ARROWS[i - 1].angle += angleArr.computeOffsetAngleArrow(i);
         }
-        return color;
+    }
+    
+    /**
+     * Draw the center of the clock
+     * @date 2022-02-02
+     * @returns {void}
+     */
+    function drawCenter() 
+    {
+        context.save();
+        context.beginPath();
+        context.fillStyle = "#BA8C63";
+        context.arc(MAX_WIDTH / 2, MAX_HEIGHT / 2, 10, 0, 2 * Math.PI, true); 
+        context.fill();
+        context.restore();
     }
 
+
+    /**
+     * Draw the clock
+     * @date 2022-02-02
+     * @returns {void}
+     */
     function drawClock() 
     {
         drawRound();
-        // drawNumbers();
+        drawLines();
+        drawArrows();
+        drawCenter();
     }
 
-    const colorArrows = Array.from(Array(10), (_e, _i) => getRandomColor());
-    const angleArrows = Array(10).fill(0);
-    const angleArr = angleArrow();
+    /**
+     * Lanch the animation of the clock
+     * @date 2022-02-02
+     * @returns {any}
+     */
     function animationClock() 
     {
         context.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
         drawClock();
-        angleArr.variationArrow();
-        for (let i = 1; i <= angleArrows.length; i++) {
-            moveArrow(angleArrows[i - 1], colorArrows[i - 1]);
-            angleArrows[i - 1] += angleArr.computeOffsetAngleArrow(i);
-        }
         window.requestAnimationFrame(animationClock);
-        drawCenter();
     }
 
+    // Start
     window.requestAnimationFrame(animationClock);
 }
