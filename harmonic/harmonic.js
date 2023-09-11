87ad496bf22f8dd6d2af02a4187f7437f164d41a
ThibWill <thibault.willer@gmail.com>
2023-09-11 16:21:00 +0200

    📝 Start skeleton code

--- harmonic/harmonic.js
+++ harmonic/harmonic.js
@@ -3,6 +3,66 @@ const initCanvas = (windowElement, canvasElement, widthScreen, heightScreen) => 
   canvasElement.width = widthScreen > 1080 ? 1080 : widthScreen;
 }
 
+const harmonicModel = {
+  number_balls: 5,
+  speed : 1,
+  // ball => xPos (percent), offset (ms)
+  balls: []
+}
+
+function controllerHarmonic() {
+  function initSystem () {
+    // draw initvue
+    return null;
+  }
+
+  function definitionFunctionMovementBall (xPosBall) {
+    return () => xPosBall;
+  }
+
+  function calculateBallPosition (xPosBall) {
+    return 0;
+  }
+
+  function calculateBallsPosition (balls) {
+    return [];
+  }
+
+  function lifeHarmonic () {
+    // update model
+    // update vue
+    return null;
+  }
+
+  return {
+    initSystem,
+    definitionFunctionMovementBall,
+    calculateBallsPosition,
+    calculateBallPosition,
+    lifeHarmonic
+  } 
+}
+
+function canvasOperations(canvas) {
+  function drawStatic () {
+
+  }
+
+  function drawBall (xPos, yPos) {
+
+  }
+
+  function erase () {
+
+  }
+
+  return {
+    drawStatic,
+    drawBall,
+    erase
+  }
+}
+
 
 window.onload = () => {
   const MAX_WIDTH = window.innerWidth;
@@ -10,4 +70,5 @@ window.onload = () => {
   
   const canavsHarmonic = document.getElementById('harmonic')
   initCanvas(window, canavsHarmonic, MAX_WIDTH, MAX_HEIGHT)
+
 }
